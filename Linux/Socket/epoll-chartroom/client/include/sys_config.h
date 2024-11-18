0db32d27bf173076bea6b5fb50baa5541b49f234
bplishuang <15007197464@163.com>
2024-11-18 11:20:09 +0800

    epoll-chatroom

---
+++ Linux/Socket/epoll-chartroom/client/include/sys_config.h
@@ -0,0 +1,15 @@
+#ifndef __SYSCONFIG_H 
+#define __SYSCONFIG_H
+
+typedef unsigned char  uint8;            		// defined for unsigned 8-bits integer variable		无符号8位整型变量  
+typedef signed   char  int8;              		// defined for signed 8-bits integer variable		有符号8位整型变量  
+typedef unsigned short uint16;             		// defined for unsigned 16-bits integer variable		无符号16位整型变量
+typedef signed   short int16;              		// defined for signed 16-bits integer variable		有符号16位整型变量
+typedef unsigned int   uint32;             		// defined for unsigned 32-bits integer variable		无符号32位整型变量 
+typedef signed   int   int32;              		// defined for signed 32-bits integer variable		有符号32位整型变量
+typedef unsigned long long  uint64;            	// defined for unsigned 64-bits integer variable		无符号64位整型变量 
+typedef signed long long int64;					// defined for unsigned 64-bits integer variable		有符号64位整型变量
+typedef unsigned long long lbaint_t;
+
+
+#endif
